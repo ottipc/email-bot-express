0629a4a95dd6b12bd79764cfc8d68113c22caa2b
Otti B <ottavio.braun@googlemail.com>
2024-12-04 13:56:25 +0100

    DELETE listener model

--- express-backend/src/models/listenerModel.js
+++
@@ -1,8 +0,0 @@
-// src/models/listenerModel.js
-const mongoose = require("mongoose");
-
-const listenerSchema = new mongoose.Schema({
-    isActive: { type: Boolean, required: true, default: true },
-});
-
-module.exports = mongoose.model("Listener", listenerSchema);
